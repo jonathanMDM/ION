470e92f0495a86c1aca44699d89479cc61abadd7
jonathan montes diaz <jonathanm@MacBook-Pro-de-jonathan.local>
2025-12-18 08:04:49 -0500

    fix: configure Sanctum guard and add HasApiTokens trait to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use App\Traits\LogActivity;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, LogActivity, \App\Traits\BelongsToCompany;
+    use HasFactory, Notifiable, HasApiTokens, LogActivity, \App\Traits\BelongsToCompany;
 
     /**
      * The "booted" method of the model.
