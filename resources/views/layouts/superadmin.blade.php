506999364e65b2df1e16e7856604ac909803523a
jonathan montes diaz <jonathanm@MacBook-Pro-de-jonathan.local>
2025-12-23 12:50:33 -0500

    Reducir más el padding del contenido en superadmin

--- resources/views/layouts/superadmin.blade.php
+++ resources/views/layouts/superadmin.blade.php
@@ -248,7 +248,7 @@
         </div>
 
         <!-- Content Area -->
-        <div class="px-4 md:px-6 py-2 md:py-4">
+        <div class="px-4 md:px-6 py-1 md:py-2">
             @yield('content')
         </div>
         </div>
